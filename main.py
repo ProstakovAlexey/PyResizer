e51832907ec8b594b6566e140b2ee48bd01162cf
Dima <crazy-dzmitry@mail.ry>
2017-03-25 19:56:52 +0200

    writing resize-function

--- main.py
+++ main.py
@@ -7,11 +7,40 @@
 
 import sys
 from PIL import Image
+import logging
+import logging.handlers
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
     QAction, QFileDialog, QApplication, QLabel)
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+###############################################
+#### LOGGING CLASS SETTINGS (py25+, py30+) ####
+###############################################
+
+f = logging.Formatter(fmt='%(levelname)s:%(name)s: %(message)s '
+    '(%(asctime)s; %(filename)s:%(lineno)d)',
+    datefmt="%Y-%m-%d %H:%M:%S")
+handlers = [
+    logging.handlers.RotatingFileHandler('logging/logfile.log', encoding='utf8',
+        maxBytes=100000, backupCount=1),
+    logging.StreamHandler()
+]
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+for h in handlers:
+    h.setFormatter(f)
+    h.setLevel(logging.DEBUG)
+    root_logger.addHandler(h)
+
+##############################
+#### END LOGGING SETTINGS ####
+##############################
+
 
 styleSheetPath = "qss/style.stylesheet"
 
@@ -20,9 +49,8 @@ class QCustomWidget(QWidget):
          super(QCustomWidget, self).__init__(parent)
          self.setAcceptDrops(True)
 
-         self.mimeQLabel = QtWidgets.QWidget()
+         self.mimeQLabel = QtWidgets.QLabel('Drag image here')
          self.mimeQLabel.setObjectName("mimeQLabel")
-         print(self.mimeQLabel.getContentsMargins)
          allQHBoxLayout = QtWidgets.QHBoxLayout()
          allQHBoxLayout.addWidget(self.mimeQLabel)
          self.setLayout(allQHBoxLayout)
@@ -37,7 +65,14 @@ class QCustomWidget(QWidget):
              print("faled")
 
      def dropEvent(self, e):
-         print(e.mimeData().text())
+         global paths_alist
+         data_raw = e.mimeData().urls()
+         paths_alist = []
+         for i in data_raw:
+              paths_alist.append(i.toString())
+         print(paths_alist)
+         
+         
 
 
 
@@ -45,11 +80,19 @@ class Example(QWidget):
 
     def __init__(self):
         super().__init__()
+        
+        self.title = 'PyResizer'
+        self.left = 300
+        self.top = 300
+        self.width = 300
+        self.height = 300
 
         self.init_ui()
 
     def init_ui(self):
-        self.resize(300, 300)
+        #self.resize(300, 300)
+        #self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
         #self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.name_programm = QtWidgets.QLabel('PyResizer')
         self.icon = QtWidgets.QPushButton('icon')
@@ -57,7 +100,10 @@ class Example(QWidget):
         self.minimize_button = QtWidgets.QPushButton('_')
 
         self.width_lineEdit = QtWidgets.QLineEdit()
+        self.width_lineEdit.setValidator(QIntValidator())
+        
         self.height_lineEdit = QtWidgets.QLineEdit()
+        self.height_lineEdit.setValidator(QIntValidator())
           
         self.convert_button = QtWidgets.QPushButton('Convert')
 
@@ -96,13 +142,15 @@ class Example(QWidget):
         v_main_box.addLayout(h_field_box)
         v_main_box.addLayout(h_size_box)
         v_main_box.addLayout(h_button_box)
+        v_main_box.setContentsMargins(5,5,5,5)
         
         
         
         
         self.setLayout(v_main_box)
         
-        
+        """connecting buttons"""
+        self.convert_button.clicked.connect(self.function_convert)
 
 
         #load stylesheets
@@ -111,6 +159,21 @@ class Example(QWidget):
 
         self.show()
   
+    def function_convert(self):
+        try:
+             width = int(self.width_lineEdit.text())
+             height = int(self.height_lineEdit.text())
+        except:
+             print("enter size")
+        else:
+             size = (width, height)
+             image = Image.open('logo.jpg')
+             resized_image = image.resize(size, Image.ANTIALIAS)
+             resized_image.save('test_image.jpg')
+             print("done")
+             #print(paths_alist[0])
+        
+
 
 if __name__ == '__main__':
 
